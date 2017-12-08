d4cc4979b4ff03fc30148097a31d256bee8fefae
Hack Reactor Student <student@hackreactor.com>
2017-12-08 12:11:42 -0800

    skeleton functions in place, passing 8/12 tests.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,49 @@
+var app = {
+  server: 'http://parse.sfm8.hackreactor.com',
 
-var postMessage = function(message) {
+	init: function() {
+	},
 	
-	$.ajax({
-	  // This is the url you should use to communicate with the parse API server.
-	  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-	  type: 'POST',
-	  data: JSON.stringify(message),
-	  contentType: 'application/json',
-	  success: function (data) {
-	    console.log('chatterbox: Message sent');
-	  },
-	  error: function (data) {
-	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-	    console.error('chatterbox: Failed to send message', data);
-	  }
-	});
+	send: function(message) {
+		
+	  $.ajax({
+		// This is the url you should use to communicate with the parse API server.
+		  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+		  type: 'POST',
+		  data: message,
+	    contentType: 'application/json',
+	  	success: function (data) {
+  		  console.log('chatterbox: Message sent');
+		  },
+		  error: function (data) {
+		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+	  	  console.error(`chatterbox: Failed to send message: ${data}`);
+		  }
+	  });
+  },
 
-}
+	fetch: function() {
+	this.server = `${this.server}/chatterbox/classes/messages`; // this is hacky. Figure out what they really want here.
+	$.ajax({
+		url: this.server,
+		type: 'GET',
+		data: null,
+		contentType: 'application/json',
+		success: function (data) {
+			console.log(`Fetched`);
+		}, 
+		error: function () {
+			console.log(`Unable to fetch.`);
+		}
+	});
+	},
+	
+	clearMessages: function() {
+	},
+
+	renderMessage: function() {
+	},
+
+	renderRoom: function() {
+	}
+};
