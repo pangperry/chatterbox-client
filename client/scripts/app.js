6d666cac6af648026770a9a04acb6094c6b050d4
Hack Reactor Student <student@hackreactor.com>
2017-12-08 17:29:09 -0800

    built features. TODO skip-limit, friendlist, sanitize input, test send.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,60 +1,75 @@
 var app = {
   server: 'http://parse.sfm8.hackreactor.com',
+  rooms: [],
+	// friends: [],
+  messages: [],
+  toSkip: 0,
 
-	init: function() {
-	},
+  init: function() {
+    $.get(`${app.server}/chatterbox/classes/messages`, function(data) {
+      app.messages = data.results;
+      app.messages.forEach(function(message) {
+        message.roomname = message.roomname ? message.roomname : 'lobby';
+        if (!app.rooms.includes(message.roomname)) {
+          app.rooms.push(message.roomname);
+          $('#roomSelector').append(`<option>${message.roomname}</option>`);
+        }
+        app.renderMessage(message);          
+      });
+// app.showMessages(data.results.slice(0, 10));
+    });
+  },
 	
-	send: function(message) {
-		
-	  $.ajax({
+  send: function(message) { 
+    $.ajax({
 		// This is the url you should use to communicate with the parse API server.
-		  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-		  type: 'POST',
-		  data: message,
-	    contentType: 'application/json',
-	  	success: function (data) {
-  		  console.log('chatterbox: Message sent');
-		  },
-		  error: function (data) {
-		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-	  	  console.error(`chatterbox: Failed to send message: ${data}`);
-		  }
-	  });
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error(`chatterbox: Failed to send message: ${data}`);
+      }
+    });
   },
 
-	fetch: function() {
-var d;
-		this.server = `${this.server}/chatterbox/classes/messages`;
-	  return $.get( `${this.server}`, function(data) {
-			return data.results
-			console.log(`data ${data.results}`)
-	});
-	// this.server = `${this.server}/chatterbox/classes/messages`; // this is hacky. Figure out what they really want here.
-	// var response = $.ajax({
-	// 	url: this.server,
-	// 	type: 'GET',
-	// 	success: function (data) {
-	// 		console.log('Fetched', data);
-	// 	}, 
-	// 	error: function () {
-	// 		console.log('Unable to fetch.');
-	// 	}
-	// });
-	// return response
-	},
+  fetch: function(callback) {
+    app.server = `${app.server}/chatterbox/classes/messages?limit=${limit}&skip=${app.toSkip}`;
+    $.get( `${app.server}`, callback);
+  },
 	
-	clearMessages: function() {
-	},
+  clearMessages: function() {
+    $('.messageFeed').html('');
+  },
 
-	renderMessage: function(message) {
-		$('.result').append(`<div class="message">${message.text}</div>`)
-		
-	},
+  renderMessage: function(message) {
+	//	$('.result').append(`<div class="message">${message.text}</div>`);
+    var userName = message.username || 'Anonymous';
+    var roomName = message.roomname || 'Main';
+    var text = message.text;
+    var createdAt = message.createdAt;
+    $('.messageFeed').append(`<div class="message">${userName}<br>${text}<br>${createdAt}</div>`);   
+  },
 
-	renderRoom: function() {
-	},
-
+  renderRoom: function() {
+    app.clearMessages();
+    app.messages.forEach(function(message) {
+      if (message.roomname === $('#roomSelector')[0].value) {
+        app.renderMessage(message);
+      }
+    });
+  },
+    
 };
+
+$(function() {
+  app.init();
+});
+
 
 
 
